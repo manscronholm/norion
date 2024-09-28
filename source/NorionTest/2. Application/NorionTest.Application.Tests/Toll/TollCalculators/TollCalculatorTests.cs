12b57b0fe81f30a7a445b45230c82a04845147d3
Måns Cronholm <momandersson@gmail.com>
2024-09-28 18:38:27 +0200

    Update tests for TollCalculator

--- source/NorionTest/2. Application/NorionTest.Application.Tests/Toll/TollCalculators/TollCalculatorTests.cs
+++ source/NorionTest/2. Application/NorionTest.Application.Tests/Toll/TollCalculators/TollCalculatorTests.cs
@@ -1,80 +1,144 @@
 ﻿using FluentAssertions;
-using NorionTest.Application.Toll.Models;
+using NorionTest.Application.Toll.Interfaces;
 using NorionTest.Application.Toll.TollCalculators;
+using NorionTest.Application.Toll.TollCalculators.Interfaces;
 using NorionTest.Domain;
-using NorionTest.Domain.Interfaces;
 using NSubstitute;
 
 namespace NorionTest.Application.Tests.Toll.TollCalculators;
 
 public class TollCalculatorTests
 {
-    [Theory]
-    [InlineData(TollFreeVehicles.Motorbike)]
-    [InlineData(TollFreeVehicles.Tractor)]
-    [InlineData(TollFreeVehicles.Emergency)]
-    [InlineData(TollFreeVehicles.Diplomat)]
-    [InlineData(TollFreeVehicles.Foreign)]
-    [InlineData(TollFreeVehicles.Military)]
-    public void GetTollFee_WithTollFreeVehicle_ShouldReturnZero(TollFreeVehicles tollFreeVehicle)
-    {
-        var vehicleMock = Substitute.For<IVehicle>();
-        vehicleMock.GetVehicleType().Returns(Enum.GetName(typeof(TollFreeVehicles), tollFreeVehicle));
-        var sut = new TollCalculator(null, null);
+    private readonly ITollFeeCalculator _tollFeeCalculator;
+    private readonly ITollFreeDateEvaluator _tollFreeDateEvaluator;
+    private readonly TollCalculator _tollCalculator;
 
-        var result = sut.GetTollFee(vehicleMock, GetNonTollFreeDates());
+    public TollCalculatorTests()
+    {
+        _tollFeeCalculator = Substitute.For<ITollFeeCalculator>();
+        _tollFreeDateEvaluator = Substitute.For<ITollFreeDateEvaluator>();
+        _tollCalculator = new TollCalculator(_tollFeeCalculator, _tollFreeDateEvaluator);
+    }
+
+    [Fact]
+    public void GetTollFee_WithMultipleDatesInSameHour_ShouldAggregateFees()
+    {
+        var vehicle = new Car();
+        var dates = new[]
+        {
+            new DateTime(2023, 7, 1, 8, 0, 0),
+            new DateTime(2023, 7, 1, 12, 00, 0),
+            new DateTime(2023, 7, 1, 16, 00, 0)
+        };
+
+        _tollFreeDateEvaluator.IsTollFreeDate(Arg.Any<DateTime>()).Returns(false);
+        
+        _tollFeeCalculator.CalculateTollFee(dates[0]).Returns(10);
+        _tollFeeCalculator.CalculateTollFee(dates[1]).Returns(10);
+        _tollFeeCalculator.CalculateTollFee(dates[2]).Returns(10);
+
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
+
+        result.Should().Be(30);
+    }
+
+    [Fact]
+    public void GetTollFee_WithDatesMoreThanAnHourApart_ShouldCountEachFee()
+    {
+        var vehicle = new Car();
+        var dates = new[]
+        {
+            new DateTime(2023, 7, 1, 10, 1, 0),
+            new DateTime(2023, 7, 1, 11, 2, 0),
+            new DateTime(2023, 7, 1, 12, 3, 0)
+        };
+
+        _tollFreeDateEvaluator.IsTollFreeDate(Arg.Any<DateTime>()).Returns(false);
+        
+        _tollFeeCalculator.CalculateTollFee(dates[0]).Returns(10);
+        _tollFeeCalculator.CalculateTollFee(dates[1]).Returns(20);
+        _tollFeeCalculator.CalculateTollFee(dates[2]).Returns(30);
+
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
+
+        result.Should().Be(60); 
+    }
+
+    [Fact]
+    public void GetTollFee_WithNoDates_ShouldReturnZero()
+    {
+        var vehicle = new Car();
+        var dates = Array.Empty<DateTime>();
+
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
 
         result.Should().Be(0);
     }
 
     [Fact]
-    public void GetTollFee_WithNonTollFreeVehicle_ShouldReturnFee()
+    public void GetTollFee_WithSingleDate_ShouldReturnFee()
     {
-        var car = new Car();
-        var sut = new TollCalculator(null, null);
+        var vehicle = new Car();
+        var date = new DateTime(2023, 7, 1, 10, 0, 0);
+        var dates = new[] { date };
 
-        var result = sut.GetTollFee(car, GetNonTollFreeDates());
+        _tollFreeDateEvaluator.IsTollFreeDate(date).Returns(false);
+        _tollFeeCalculator.CalculateTollFee(date).Returns(10);
 
-        result.Should().BePositive(null);
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
+
+        result.Should().Be(10);
     }
 
     [Fact]
-    public void GetTollFee_DuringTollFreeDate_ShouldReturnZero()
+    public void GetTollFee_WithTollFreeDate_ShouldReturnZero()
     {
-        var car = new Car();
-        var sut = new TollCalculator(null, null);
+        var vehicle = new Car();
+        var date = new DateTime(2023, 7, 1);
+        var dates = new[] { date };
 
-        var result = sut.GetTollFee(car, GetTollFreeDates());
+        _tollFreeDateEvaluator.IsTollFreeDate(date).Returns(true);
+
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
 
         result.Should().Be(0);
     }
 
     [Fact]
-    public void GetTollFee_DuringNonTollFreeDate_ShouldReturnFee()
+    public void GetTollFee_WithTollFreeVehicle_ShouldReturnZero()
     {
-        var car = new Car();
-        var sut = new TollCalculator(null, null);
+        var vehicle = new Motorbike();
+        var date = new DateTime(2023, 7, 1);
+        var dates = new[] { date };
 
-        var result = sut.GetTollFee(car, GetNonTollFreeDates());
+        _tollFreeDateEvaluator.IsTollFreeDate(date).Returns(false);
 
-        result.Should().BePositive();
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
+
+        result.Should().Be(0);
     }
 
-    private static DateTime[] GetNonTollFreeDates()
+    [Fact]
+    public void GetTollFee_WithFeesExceedingSixty_ShouldCapAtSixty()
     {
-        var nonTollFreeTime = new TimeOnly(7, 0);
-        return
-        [
-            new DateTime(new DateOnly(2024, 01, 02), nonTollFreeTime),
-            new DateTime(new DateOnly(2024, 01, 03), nonTollFreeTime),
-            new DateTime(new DateOnly(2024, 01, 04), nonTollFreeTime) 
-        ];
-    }
+        var vehicle = new Car();
+        var dates = new[]
+        {
+            new DateTime(2023, 7, 1, 10, 0, 0),
+            new DateTime(2023, 7, 1, 12, 0, 0),
+            new DateTime(2023, 7, 1, 14, 0, 0),
+            new DateTime(2023, 7, 1, 16, 0, 0)
+        };
 
-    private static DateTime[] GetTollFreeDates() =>
-    [
-        new(2013, 12, 24),
-        new(2013, 12, 25),
-        new(2013, 12, 26)
-    ];
+        _tollFreeDateEvaluator.IsTollFreeDate(Arg.Any<DateTime>()).Returns(false);
+        
+        _tollFeeCalculator.CalculateTollFee(dates[0]).Returns(20);
+        _tollFeeCalculator.CalculateTollFee(dates[1]).Returns(20);
+        _tollFeeCalculator.CalculateTollFee(dates[2]).Returns(20);
+        _tollFeeCalculator.CalculateTollFee(dates[3]).Returns(20);
+
+        var result = _tollCalculator.GetTollFee(vehicle, dates);
+
+        result.Should().Be(60);
+    }
 }
